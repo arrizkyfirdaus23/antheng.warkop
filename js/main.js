6c64d278c2fba748b4932648b9ca4969b5cd3a99
arrizkyfirdaus23 <arrizkyfirdaus2308@gmail.com>
2026-09-07 16:01:12 +0700

    0.2 Version
    Unggah JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+            }
+        });
+    }, {
+        rootMargin: '0px 0px -100px 0px'
+    });
 
+    const elementsToAnimate = document.querySelectorAll(
+        '.service-item, .why-item, .gallery-item, .vm-item, .commitment-list li, .service-detail-intro, .benefits-column, .process-column'
+    );
+
+    elementsToAnimate.forEach(el => observer.observe(el));
+});
